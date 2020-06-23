e5479d0d540c1864e8da425cbd44877bfd6877b6
camila queiroz <queiroz.cami@outlook.com>
2020-06-23 19:29:21 -0300

    Organizando o index.js file

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const targets = document.querySelectorAll('[data-anime]');
  if(targets) {
     window.addEventListener ('scroll', () => {
-      if(window.scrollY + 500 >= window.innerHeight){
+      if(window.scrollY + 400 >= window.innerHeight){
          targets.forEach((target) => target.classList.add('animate'));
        } else {
          targets.forEach((target) => target.classList.remove('animate'));
        }
     });
   };
-
-
-// export { animeScroll };
