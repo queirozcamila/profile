cf47a88efcd96d2c6638f9e935629664898f2491
camila queiroz <queiroz.cami@outlook.com>
2020-06-23 19:10:36 -0300

    Adicionando animação no coding

---
+++ index.js
@@ -0,0 +1,13 @@
+const targets = document.querySelectorAll('[data-anime]');
+ if(targets) {
+    window.addEventListener ('scroll', () => {
+      if(window.scrollY + 500 >= window.innerHeight){
+         targets.forEach((target) => target.classList.add('animate'));
+       } else {
+         targets.forEach((target) => target.classList.remove('animate'));
+       }
+    });
+  };
+
+
+// export { animeScroll };
