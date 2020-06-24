547ee70ce885676462d6263c66a9833b3a2526b1
camila queiroz <queiroz.cami@outlook.com>
2020-06-23 23:07:15 -0300

    Alterando todo o layout do profile e inserindo conteudo

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const targets = document.querySelectorAll('[data-anime]');
  if(targets) {
     window.addEventListener ('scroll', () => {
-      if(window.scrollY + 400 >= window.innerHeight){
+      if(window.scrollY >= window.innerHeight){
          targets.forEach((target) => target.classList.add('animate'));
        } else {
          targets.forEach((target) => target.classList.remove('animate'));
